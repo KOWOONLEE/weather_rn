9f6cd33eb0bc80e467b2fc3856b92c2497d17e88
KOWOONLEE <hhh3901@gmail.com>
2023-01-30 17:40:55 +0900

    Rev: 날씨 어플 (온도, 날짜, 날씨)

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
 
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "2d9ebcd9cdf9b6b9a007f04426ef4ccb";
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState(null);
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     // console.log(permission); // granted:true-> 허락받음
     if (!granted) {
@@ -25,9 +33,27 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(
+      json.list.filter((weather) => {
+        if (weather.dt_txt.includes("00:00:00")) {
+          return weather;
+        }
+      })
+    );
+    // console.log(
+    //   json.list.filter((weather) => {
+    //     if (weather.dt_txt.includes("00:00:00")) {
+    //       return weather;
+    //     }
+    //   })
+    // );
   };
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
   return (
     <View style={styles.container}>
@@ -41,20 +67,22 @@ export default function App() {
         // showsHorizontalScrollIndicator={false} 하단에 스크롤 바 숨기고 싶을 떄, 나는 보이게 함
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.descript}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.descript}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.descript}>Sunny</Text>
-        </View>
-
-        <View></View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size="large" />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.daily}>{day.dt_txt.substr(5, 5)}</Text>
+              <Text style={styles.temp}>
+                {parseFloat(day.main.temp).toFixed(1)}°C
+              </Text>
+              <Text style={styles.descript}>{day.weather[0].main}</Text>
+              <Text style={styles.tinydes}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -79,14 +107,20 @@ const styles = StyleSheet.create({
     //day 하나당 화면 하나에 채우고 싶어서 dementions api 사용함. 변수화해서 가져옴
     width: SCREEN_WIDTH,
     alignItems: "center",
-    backgroundColor: "orange",
+  },
+  daily: {
+    marginTop: 10,
+    fontSize: 60,
   },
   temp: {
-    marginTop: 50,
-    fontSize: 170,
+    fontSize: 100,
   },
   descript: {
-    marginTop: -30,
+    marginTop: 0,
     fontSize: 60,
+  },
+  tinydes: {
+    marginTop: 10,
+    fontSize: 30,
   },
 });
